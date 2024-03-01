dbfaca7effefc985101f280d995321c3b0e84ffb
xander konell <xanderkonell@gmail.com>
2024-03-01 15:48:19 -0600

    saved changes

--- Problem#6/contactlist.py
+++ Problem#6/contactlist.py
@@ -9,7 +9,7 @@ def process_user_contacts(user_input):
         user_contacts[tokens[i]] = tokens[i + 1]
     # Get contact name from input, output contact's phone number
     contact_name = input("Enter the contact name: ")
-    print(user_contacts.get(contact_name, "Contact not found"))
+    print(user_contacts.get(contact_name, "Contact not found."))
 
    
 if __name__ == '__main__':
